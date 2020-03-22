9e4a2c17fe2c13b99b211181c7b46d3fa4fda146
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-03-23 00:02:27 +0300

    feat: added buttons

--- flutter_quiz/lib/main.dart
+++ flutter_quiz/lib/main.dart
@@ -6,7 +6,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext buildContext) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('AppBar')),
+        body: Column(
+          children: <Widget>[
+            Text('Hello world!'),
+            RaisedButton(
+              onPressed: () {
+                print('Answer 1');
+              },
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print('Answer 2');
+              },
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print('Answer 3');
+              },
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
