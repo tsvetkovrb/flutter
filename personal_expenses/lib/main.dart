f4928bb678031f1e190fb8851a56bacbd1918e01
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-03-30 23:24:29 +0300

    feat: added external deps

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'model/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -64,7 +65,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                           padding: EdgeInsets.all(10),
                           child: Text(
-                            transaction.amount.toString(),
+                            '\$${transaction.amount}',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 20,
@@ -88,7 +89,7 @@ class MyHomePage extends StatelessWidget {
                               ),
                             ),
                             Text(
-                              transaction.date.toString(),
+                              DateFormat.yMMMd().format(transaction.date),
                               style: TextStyle(
                                 color: Colors.grey,
                               ),
